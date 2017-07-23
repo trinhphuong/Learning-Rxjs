39002cb1987c9b486dd7b2f0e2d71c443e7cb42c
Trinh Thanh Phuong <iphuongtt@gmail.com>
2017-07-23 16:02:35 +0700

    Going async with setTimeout

--- main.ts
+++ main.ts
@@ -3,10 +3,18 @@ import { Observable, Observer } from 'rxjs';
 let numbers = [1, 5, 10];
 
 let source = Observable.create(observer => {
-    for (let n of numbers) {
-        observer.next(n);
+    let index = 0;
+    let produceValue = () => {
+        observer.next(numbers[index++]);
+
+        if (index < numbers.length) {
+            setTimeout(produceValue, 2000);
+        } else {
+            observer.complete();
+        }
     }
-    observer.complete();
+
+    produceValue();
 });
 
 
